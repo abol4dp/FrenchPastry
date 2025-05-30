ebd703e898e91644a5df0811130e2cfb1b4adc2f
Abolfazl Khodabande <abolfazllkhodabande@gmail.com>
2025-05-30 13:15:09 +0330

    Create LoginApiService

---
+++ app/src/main/java/com/example/mynotes/mynotes/mynotes/frenchpastry_kh/login/retrifit/LoginApiService.kt
@@ -0,0 +1,41 @@
+package com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit
+
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.SendCodeData
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.VerifyCodeData
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.homemodel.HomeResponse
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface LoginApiService {
+
+    @GET("v1/main")
+    suspend fun getMain(): Response<HomeResponse>
+
+
+
+    @FormUrlEncoded
+    @POST("v1/auth/phone/login")
+    suspend fun sendCodeWithEmail(
+        @Header("app-device-uid")deviceId:String,
+        @Header("app-public-key")publicId:String,
+        @Field("phone") phone:String
+    ):Response<SendCodeData>
+
+
+
+
+    @FormUrlEncoded
+    @POST("v1/auth/phone/login/verify")
+    suspend fun verifyCode(
+        @Header("app-device-uid")deviceId:String,
+        @Header("app-public-key")publicId:String,
+        @Field("code") code:String,
+        @Field("phone") phone:String,
+    ):Response<VerifyCodeData>
+
+
+}
