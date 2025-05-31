c883957ac316719a7dc8b422ee71e3a49e72a781
Abolfazl Khodabande <abolfazllkhodabande@gmail.com>
2025-05-31 12:26:38 +0330

    Create Repository Class and fix Di

---
+++ app/src/main/java/com/example/mynotes/mynotes/mynotes/frenchpastry_kh/login/retrifit/LoginApiRepository.kt
@@ -0,0 +1,12 @@
+package com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit
+
+import javax.inject.Inject
+
+class LoginApiRepository @Inject constructor (
+
+private val apiService: LoginApiService
+
+) {
+
+
+}
