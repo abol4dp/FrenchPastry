3e30a6f901d8c3d8cf646dfa41ac169b9774d952
Abolfazl Khodabande <abolfazllkhodabande@gmail.com>
2025-06-01 15:52:40 +0330

    Fix getMain() to fetch HomeResponse data

--- app/src/main/java/com/example/mynotes/mynotes/mynotes/frenchpastry_kh/login/retrifit/LoginApiRepository.kt
+++ app/src/main/java/com/example/mynotes/mynotes/mynotes/frenchpastry_kh/login/retrifit/LoginApiRepository.kt
@@ -1,12 +1,49 @@
 package com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit
 
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.SendCodeData
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.VerifyCodeData
+import com.example.mynotes.mynotes.mynotes.frenchpastry_kh.login.retrifit.model.homemodel.HomeResponse
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
-class LoginApiRepository @Inject constructor (
-
-private val apiService: LoginApiService
+class LoginApiRepository @Inject constructor(
+    private val apiService: LoginApiService
 
 ) {
+    val main = MutableStateFlow<HomeResponse>(HomeResponse())
+    val verifyCodeResponse = MutableStateFlow<VerifyCodeData>(VerifyCodeData())
+    val sendCodResponse = MutableStateFlow<SendCodeData>(SendCodeData())
+    val loading = MutableStateFlow(false)
+    val errorVerifyCode = MutableStateFlow(false)
+
+
+suspend fun getMain(){
+val response = try {
+
+    apiService.getMain()
+
+}catch (e : Exception){
+    return
+}
+if (response.isSuccessful){
+val body = response.body()
+    body.let {
+        main.let { it }
+
+    }
+
+
+}
+
+
+
+
+
+
+}
+
+
+
 
 
 }
